d253096399b6e62dd4b149f033258ffc06727164
Uriel Olmedo <uriel.olme@gmail.com>
2022-12-05 04:42:17 -0300

    add component Hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import NavBar from './Components/NavBar'
 import './App.css'
+import Hero from './Components/Hero'
 
 function App() {
 
   return (
     <main className="App">
       <NavBar />
+      <Hero />
     </main>
   )
 }
--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,7 +1,32 @@
 import React from 'react'
+import '../Styles/Hero.css'
+import { FiDownload } from 'react-icons/fi'
 
 export default function Hero() {
   return (
-    <div>Hero</div>
+    <section id='home' className='hero'>
+
+      <div className='container heroWrap'>
+
+        <img src='./binary.svg' className='heroImg' />
+
+        <div className='heroText'>
+          <h1>Hola, soy Uriel.</h1>
+          <span>Javascript Fullstack Developer</span>
+
+          <div className='heroBtnGroup'>
+            <a href='#contact' className='btn'>Contactame</a>
+            <a href='#' className='btn'>  Cv <FiDownload size='1.5rem'/></a>
+          </div>
+
+        </div>
+
+      </div>
+
+      <div className='wave' style={{ height: '150px', overflow: 'hidden' }}>
+      <svg viewBox='0 0 500 150' preserveAspectRatio='none' style={{ height: '100%', width: '100%' }}><path d='M-3.72,92.26 C150.00,150.00 361.40,-57.73 514.33,99.18 L500.00,150.00 L0.00,150.00 Z' style={{ stroke: 'none', fill: 'var(--light)' }} /></svg>
+      </div>
+
+    </section>
   )
 }
